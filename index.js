bef14d22e809295901c05ef44008fdeefef2ca4d
“ash” <ashrafha@umich.edu>
2023-02-19 18:08:44 -0500

    Add get tweets api

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import fetch from "node-fetch";
 const PORT = 8080;
 
-app.use(express.json());
+const app = express();
+
+// Get all tweets (create time, id, and tweet text) available in the archive.
+app.get("/api/tweets", (req, res) => {
+  fetch("https://foyzulhassan.github.io/files/favs.json")
+    .then((res) => res.json())
+    .then((data) => {
+      // get the created_at, id, and tweet text fields
+      let tweets = data.map((tweet) => {
+        return { created_at: tweet.created_at, id: tweet.id, text: tweet.text };
+      });
+
+      res.send(tweets);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error retrieving data");
+    });
+});
+
+// Get a list of all external links (all links that appear in the tweet text field.
+// Use regular expressions to extract the links, the links should be grouped based on tweet ids.
+
+// Get the details about a given tweet (given the tweet’s id). Details of the tweet include created_at, text, screen_name, lang.
+
+// Get detailed profile information(name, location, description) about a given Twitter user (given the user’s screen name).
 
 app.listen(PORT, () =>
   console.log(`Server is running on port http://localhost:${PORT}`)
 );
-
-// https://foyzulhassan.github.io/files/favs.json
-app.get("/user", (req, res) => {
-  res.status(200).send({
-    name: "eddie",
-    age: "23",
-  });
-});
