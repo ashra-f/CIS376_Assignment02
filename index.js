c3c2621b110c463249dec31cf0295cc2f5b238d5
“ash” <ashrafha@umich.edu>
2023-02-19 22:41:11 -0500

    Modify Links Retrieving

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+/*
+ *  Author: Ashraf Hammoud
+ *  Class: CIS 376
+ *  Instructor: Foyzul Hassan
+ *  Date: 02/20/2023
+ */
+
+// Server setup
 import express from "express";
 import fetch from "node-fetch";
 const PORT = 8080;
-
 const app = express();
 
-// Get all tweets (create time, id, and tweet text) available in the archive.
+/**
+ * GET a list of tweets.
+ * @route GET /api/tweets
+ * @returns {object} 200 - An array of tweets objects (create time, id, and tweet text)
+ * @returns {object} 500 - An error message
+ */
 app.get("/api/tweets", (req, res) => {
   fetch("https://foyzulhassan.github.io/files/favs.json")
     .then((res) => res.json())
     .then((data) => {
-      // get the created_at, id, and tweet text fields
+      // Get only the created_at, id, and tweet text fields from all tweets
       let tweets = data.map((tweet) => {
         return { created_at: tweet.created_at, id: tweet.id, text: tweet.text };
       });
@@ -22,13 +34,17 @@ app.get("/api/tweets", (req, res) => {
     });
 });
 
-// Get a list of all external links (all links that appear in the tweet text field.
-// Use regular expressions to extract the links, the links should be grouped based on tweet ids.
+/**
+ * GET a list of links.
+ * @route GET /api/links
+ * @returns {object} 200 - An array of links objects grouped by tweet ids
+ * @returns {object} 500 - An error message
+ */
 app.get("/api/links", (req, res) => {
   fetch("https://foyzulhassan.github.io/files/favs.json")
     .then((res) => res.json())
     .then((data) => {
-      // Gets tweets with only their id and text body
+      // Gets Tweets With Only Their Id and TextBody
       let tweetLinks = data.map((tweet) => {
         return {
           id: tweet.id,
@@ -36,19 +52,24 @@ app.get("/api/links", (req, res) => {
         };
       });
 
-      // Use regular expressions to extract the links from text body
+      // Extract Links From TextBody Using Regular Expressions
       let regex = /https?:\/\/[^\s]+/g;
       tweetLinks.forEach((tweet) => {
         let links = tweet.textBody.match(regex);
         tweet.links = links;
       });
-
-      // Remove textBody from tweets
+      // Group and Label Links by TweetId
+      let linksByTweetId = [];
       tweetLinks.forEach((tweet) => {
-        delete tweet.textBody;
+        linksByTweetId.push({ [tweet.id]: { links: tweet.links } });
       });
 
-      res.send(tweetLinks);
+      if (linksByTweetId.length === 0) {
+        res.send("No links");
+        return;
+      }
+
+      res.send(linksByTweetId);
     })
     .catch((err) => {
       console.log(err);
@@ -56,14 +77,25 @@ app.get("/api/links", (req, res) => {
     });
 });
 
-// Get the details about a given tweet (given the tweet’s id). Details of the tweet include created_at, text, screen_name, lang.
+/**
+ * GET a tweet.
+ * @route GET /api/tweet/:id
+ * @returns {object} 200 - A single tweet object (created_at, text, screen_name, lang) given a tweet Id
+ * @returns {object} 500 - An error message
+ */
 app.get("/api/tweet/:id", (req, res) => {
   fetch("https://foyzulhassan.github.io/files/favs.json")
     .then((res) => res.json())
     .then((data) => {
+      // Checks if the provided Id is a number
+      if (isNaN(req.params.id)) {
+        res.send("Invalid tweet id");
+        return;
+      }
+
       const tweetId = parseInt(req.params.id);
 
-      // get the created_at, text, screen_name, and lang fields for the specified tweet
+      // Get only the created_at, text, screen_name, and lang fields for the user selected tweet
       let selectedTweet = data
         .filter((tweet) => tweet.id === tweetId)
         .map((tweet) => {
@@ -75,6 +107,13 @@ app.get("/api/tweet/:id", (req, res) => {
           };
         });
 
+      if (selectedTweet.length === 0) {
+        res.send("Tweet not found");
+        return;
+      } else if (selectedTweet.length === 1) {
+        selectedTweet = selectedTweet[0];
+      }
+
       res.send(selectedTweet);
     })
     .catch((err) => {
@@ -83,14 +122,19 @@ app.get("/api/tweet/:id", (req, res) => {
     });
 });
 
-// Get detailed profile information(name, location, description) about a given Twitter user (given the user’s screen name).
+/**
+ * GET a user profile.
+ * @route GET /api/user/:username
+ * @returns {object} 200 - A single user profile object (name, location, description) given a username
+ * @returns {object} 500 - An error message
+ */
 app.get("/api/user/:username", (req, res) => {
   fetch("https://foyzulhassan.github.io/files/favs.json")
     .then((res) => res.json())
     .then((data) => {
       const username = req.params.username;
 
-      // get the name, location, description for the specified username
+      // Get only the name, location, description for the specified username
       let userProfile = data
         .filter((tweet) => tweet.user.screen_name === username)
         .map((tweet) => {
@@ -101,10 +145,18 @@ app.get("/api/user/:username", (req, res) => {
           };
         });
 
+      if (userProfile.length === 0) {
+        res.send("User not found");
+        return;
+      } else if (userProfile.length === 1) {
+        userProfile = userProfile[0];
+      }
+
       res.send(userProfile);
     });
 });
 
+// Set up listening port
 app.listen(PORT, () =>
   console.log(`Server is running on port http://localhost:${PORT}`)
 );
